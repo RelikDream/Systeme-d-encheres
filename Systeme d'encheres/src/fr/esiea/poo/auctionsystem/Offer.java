9ccc93efb9df022e612e2d02945d55d974e8f77b
RelikDream <bejuit@et.esiea.fr>
2013-05-12 15:29:18 +0200

    Add Offer

---
+++ Systeme d'encheres/src/fr/esiea/poo/auctionsystem/Offer.java
@@ -0,0 +1,11 @@
+package fr.esiea.poo.auctionsystem;
+
+public class Offer {
+	private String userLogin;
+	private double price;
+	public Offer(String userLogin, double price) {
+		this.userLogin=userLogin;
+		this.price=price;
+	}
+
+}

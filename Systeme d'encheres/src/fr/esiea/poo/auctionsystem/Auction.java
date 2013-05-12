370f2638083361591798d90c9e0b90c50f98df69
RelikDream <bejuit@et.esiea.fr>
2013-05-12 22:48:06 +0200

    Add ReservePrice viewers to Auction

--- Systeme d'encheres/src/fr/esiea/poo/auctionsystem/Auction.java
+++ Systeme d'encheres/src/fr/esiea/poo/auctionsystem/Auction.java
@@ -11,7 +11,7 @@ public class Auction {
 	private final double minPrice;
 	private final double reservePrice;
 	private Offer currentOffer;
-	
+
 	protected Auction(String owner,int id, Item object, double minPrice, double reservePrice,Date deadline ){
 		this.id = id;
 		this.owner=owner;
@@ -22,31 +22,31 @@ public class Auction {
 		this.state=AuctionState.CREATED;
 		this.currentOffer=new Offer("",minPrice);
 	}
-	
+
 	public AuctionState getState() {
 		return state;
 	}
 
 	public void setState(AuctionState state) throws Exception {
-	switch (state){
-	case CANCELLED:
-		if(this.state==AuctionState.FINISHED)
-			throw new Exception("You can't cancelled a finished auction");
-		break;
-	case CREATED:
-		throw new Exception("You can't change a auction to a created state");
-	case FINISHED:
-		if(this.state!=AuctionState.PUBLISHED)
-			throw new Exception("An auction need to be published before it can be finished");
-		break;
-	case PUBLISHED:
-		if(this.state!=AuctionState.CREATED)
-			throw new Exception("You need to have a newly created auction to publish it");
-		break;
-	default:
-		throw new Exception("Invalid argument");
-	
-	}
+		switch (state){
+		case CANCELLED:
+			if(this.state==AuctionState.FINISHED)
+				throw new Exception("You can't cancelled a finished auction");
+			break;
+		case CREATED:
+			throw new Exception("You can't change a auction to a created state");
+		case FINISHED:
+			if(this.state!=AuctionState.PUBLISHED)
+				throw new Exception("An auction need to be published before it can be finished");
+			break;
+		case PUBLISHED:
+			if(this.state!=AuctionState.CREATED)
+				throw new Exception("You need to have a newly created auction to publish it");
+			break;
+		default:
+			throw new Exception("Invalid argument");
+
+		}
 		this.state = state;
 	}
 
@@ -66,13 +66,19 @@ public class Auction {
 		return minPrice;
 	}
 
-	public double getReservePrice() {
-		return reservePrice;
+	public double getReservePrice(String username) throws Exception {
+		if(username.equals(this.owner))
+			return reservePrice;
+		throw new Exception("You are not the owner of this auction");
+	}
+	public boolean isReservePriceReached(){
+		return currentOffer.getPrice()>=this.reservePrice;
+				
 	}
 	public int getId(){
 		return(id);
 	}
-	
+
 	public void updateOffer(Offer o) throws Exception{
 		if(this.currentOffer.getPrice()<o.getPrice())
 			this.currentOffer=o;

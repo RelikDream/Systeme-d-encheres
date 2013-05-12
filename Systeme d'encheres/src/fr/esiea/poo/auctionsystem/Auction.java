3fa1a83e018a7fc4953c78e8566556244294f537
RelikDream <bejuit@et.esiea.fr>
2013-05-12 16:12:36 +0200

    Add Getters and Setter For Auction

--- Systeme d'encheres/src/fr/esiea/poo/auctionsystem/Auction.java
+++ Systeme d'encheres/src/fr/esiea/poo/auctionsystem/Auction.java
@@ -1,28 +1,63 @@
 package fr.esiea.poo.auctionsystem;
 
+import java.util.Date;
+
 public class Auction {
-	private int id;
-	private Item object;
-	private Object deadline;
-	private AuctionState state;
-	private double minPrice;
-	private double reservePrice;
+	private final int id;
+	private final String owner;
+	private final Item object;
 	
-	protected Auction(int id, Item object, double minPrice, double reservePrice ){
+
+
+	private final Date deadline;
+	private AuctionState state;
+	private final double minPrice;
+	private final double reservePrice;
+	
+	protected Auction(String owner,int id, Item object, double minPrice, double reservePrice,Date deadline ){
 		this.id = id;
+		this.owner=owner;
 		this.object = object;
 		this.minPrice= minPrice;
 		this.reservePrice = reservePrice;
-		//TODO
+		this.deadline=deadline;
+		this.state=AuctionState.CREATED;
 	}
 	
 	@Override
 	public String toString(){
-		//TODO
+		//TODO Tranform Auction into a String
 		return null;
 	}
 	
-	
+	public AuctionState getState() {
+		return state;
+	}
+
+	public void setState(AuctionState state) {
+		//TODO verify we can't change to fobbiden Auctions
+		this.state = state;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public Item getObject() {
+		return object;
+	}
+
+	public Date getDeadline() {
+		return deadline;
+	}
+
+	public double getMinPrice() {
+		return minPrice;
+	}
+
+	public double getReservePrice() {
+		return reservePrice;
+	}
 	public int getId(){
 		return(id);
 	}
